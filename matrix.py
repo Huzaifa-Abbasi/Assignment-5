36eccd6bcd946ae34573436869c2c6af99b215d4
Huzaifa Abbasi <151647824+Huzaifa-Abbasi@users.noreply.github.com>
2024-01-01 22:06:00 +0500

    matrix.py

--- matrix.py
+++ matrix.py
@@ -4,9 +4,9 @@ b = [[5, 6],[7, 8]]
 for i in a,b:
     print(i)
 
+for i in range(len(a)):
+    for j in range(len(a[0])):
+        c = a[i][j] + b[i][j]
+        print("the sum of matrix =", c)
 
-for i in range(1):
-    for j in range(1):
-        c=a[i][j]+b[i][j]
-        print("the sum of matrix=",c)
 print()        
